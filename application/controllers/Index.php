08cf50e17e471ba446569aae6a1a84385fed5828
fandon <fangdong0010@163.com>
2016-11-29 22:01:03 +0800

    1、添加Index

---
+++ application/controllers/Index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends CI_Controller
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function index()
+	{
+
+	}
+}
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,7 +6,7 @@ class Login extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->check_status();
+		//$this->check_status();
 	}
 	public function index()
 	{
