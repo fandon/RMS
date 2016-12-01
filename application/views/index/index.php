ba2a000ce281f9a08686070339c6ff76dfab74ad
fangd <1329307957@qq.com>
2016-12-01 17:23:16 +0800

    1、修改

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -2,75 +2,185 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
+
 <html>
-<head>
-  <title>RMS</title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/layui/css/layui.css">
-  <script type="text/javascript" src="<?php echo base_url()?>public/layui/layui.js"></script>
-  <style type="text/css">
-    #header-title{
-      font-size: 24px;
-      color: #FFF;
-      margin-left: 20px;
-      line-height: 50px;
-      width: 30%;
-    }
-    .header-right{
-      float: right;
-    }
-    .header-right img {
-      width: 50px;
-    }
-  </style>
-</head>
-<body>
-  <div class="layui-layout-admin">
-    <div class="layui-header layui-bg-black">
-      <div clas="layui-main" >
-        <span id="header-title">管理系统</span>
-        <span class="header-right">
-          <img src="<?php echo base_url()?>public/images/f.png">
-        </span>
+
+  <head>
+    <meta charset="utf-8">
+    <title>后台管理模板</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=no">
+
+    <link rel="stylesheet" href="<?php echo base_url()?>public/layui/css/layui.css" media="all" />
+    <link rel="stylesheet" href="<?php echo base_url()?>public/css/global.css" media="all">
+    <link rel="stylesheet" href="<?php echo base_url()?>public/font-awesome/css/font-awesome.min.css">
+
+  </head>
+
+  <body>
+    <div class="layui-layout layui-layout-admin">
+      <div class="layui-header header header-demo">
+        <div class="layui-main">
+          <div class="admin-login-box">
+            <a class="logo" style="left: 0;" href="http://beginner.zhengjinfan.cn/demo/beginner_admin/">
+              <span style="font-size: 22px;">BeginnerAdmin</span>
+            </a>
+            <div class="admin-side-toggle">
+              <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+          </div>
+          <ul class="layui-nav admin-header-item">
+            <li class="layui-nav-item">
+              <a href="javascript:;">清除缓存</a>
+            </li>
+            <li class="layui-nav-item">
+              <a href="javascript:;">浏览网站</a>
+            </li>
+            <li class="layui-nav-item">
+              <a href="javascript:;" class="admin-header-user">
+                <img src="images/0.jpg" />
+                <span>beginner</span>
+              </a>
+              <dl class="layui-nav-child">
+                <dd>
+                  <a href="javascript:;"><i class="fa fa-user-circle" aria-hidden="true"></i> 个人信息</a>
+                </dd>
+                <dd>
+                  <a href="javascript:;"><i class="fa fa-gear" aria-hidden="true"></i> 设置</a>
+                </dd>
+                <dd>
+                  <a href="login.html"><i class="fa fa-sign-out" aria-hidden="true"></i> 注销</a>
+                </dd>
+              </dl>
+            </li>
+          </ul>
+          <ul class="layui-nav admin-header-item-mobile">
+            <li class="layui-nav-item">
+              <a href="login.html"><i class="fa fa-sign-out" aria-hidden="true"></i> 注销</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="layui-side layui-bg-black">
-      <ul class="layui-nav layui-nav-tree">
-        <li class="layui-nav-item layui-nav-itemed">
-          <a href="javascript:void(0);">系统管理</a>
-          <dl class="layui-nav-child">
-            <dd><a href="<?php echo site_url('')?>">选项一</a></dd>
-            <dd><a href="javascript:;">选项二</a></dd>
-            <dd><a href="javascript:;">选项三</a></dd>
-            <dd><a href="">跳转项</a></dd>
-          </dl>
-        </li>
-        <li class="layui-nav-item">
-          <a href="javascript:;">解决方案</a>
-          <dl class="layui-nav-child">
-            <dd><a href="">移动模块</a></dd>
-            <dd><a href="">后台模版</a></dd>
-            <dd><a href="">电商平台</a></dd>
-          </dl>
-        </li>
-        <li class="layui-nav-item"><a href="">云市场</a></li>
-        <li class="layui-nav-item"><a href="">社区</a></li>
-      </ul>
-    </div>
-    <div class="layui-body">
+      <div class="layui-side layui-bg-black" id="admin-side">
+        <div class="layui-side-scroll" id="admin-navbar-side" lay-filter="side">
+        </div>
+      </div>
+      <div class="layui-body" style="bottom: 0;border-left: solid 2px #1AA094;" id="admin-body">
+        <div class="layui-tab admin-nav-card layui-tab-brief" lay-filter="admin-tab">
+          <ul class="layui-tab-title">
+            <li class="layui-this">
+              <i class="fa fa-dashboard" aria-hidden="true"></i>
+              <cite>控制面板</cite>
+            </li>
+          </ul>
+          <div class="layui-tab-content" style="min-height: 150px; padding: 5px 0 0 0;">
+            <div class="layui-tab-item layui-show">
+              <iframe >123</iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="layui-footer footer footer-demo" id="admin-footer">
+        <div class="layui-main">
+          <p>2016 &copy;
+            <a href="">123</a>
+          </p>
+        </div>
+      </div>
+      <div class="site-tree-mobile layui-hide">
+        <i class="layui-icon">&#xe602;</i>
+      </div>
+      <div class="site-mobile-shade"></div>
       
+      <script type="text/javascript" src="<?php echo base_url()?>public/layui/layui.js"></script>
+      <script type="text/javascript">
+          var navs = [{
+      "title": "基本元素",
+      "icon": "fa-cubes",
+      "spread": true,
+      "children": [{
+        "title": "按钮",
+        "icon": "&#xe641;",
+        "href": "button.html"
+      }, {
+        "title": "表单",
+        "icon": "&#xe63c;",
+        "href": "form.html"
+      }, {
+        "title": "表格",
+        "icon": "&#xe63c;",
+        "href": "table.html"
+      }, {
+        "title": "导航",
+        "icon": "&#xe609;",
+        "href": "nav.html"
+      }, {
+        "title": "Tab选项卡",
+        "icon": "&#xe62a;",
+        "href": "tab.html"
+      }, {
+        "title": "辅助性元素",
+        "icon": "&#xe60c;",
+        "href": "auxiliar.html"
+      }]
+    }, {
+      "title": "组件",
+      "icon": "fa-cogs",
+      "spread": false,
+      "children": [{
+        "title": "Datatable",
+        "icon": "fa-table",
+        "href": "begtable.html"
+      }, {
+        "title": "Navbar组件",
+        "icon": "fa-navicon",
+        "href": "navbar.html"
+      }]
+    }, {
+      "title": "第三方组件",
+      "icon": "&#x1002;",
+      "spread": false,
+      "children": [{
+        "title": "iCheck组件",
+        "icon": "fa-check-square-o",
+        "href": "icheck.html"
+      }]
+    }, {
+      "title": "地址本",
+      "icon": "fa-address-book",
+      "href": "",
+      "spread": true,
+      "children": [{
+        "title": "Github",
+        "icon": "fa-github",
+        "href": "https://www.github.com/"
+      }, {
+        "title": "QQ",
+        "icon": "fa-qq",
+        "href": "http://www.qq.com/"
+      }, {
+        "title": "Fly社区",
+        "icon": "&#xe609;",
+        "href": "http://fly.layui.com/"
+      }, {
+        "title": "新浪微博",
+        "icon": "fa-weibo",
+        "href": "http://weibo.com/"
+      }]
+    }, {
+      "title": "这是一级导航",
+      "icon": "fa-stop-circle",
+      "href": "https://www.baidu.com",
+      "spread": false
+    }];
+    </script>
+      <script src="<?php echo base_url()?>public/js/index.js"></script>
     </div>
-  </div>
-  <script>
-    layui.use('element', function(){
-      var element = layui.element(); //导航的hover效果、二级菜单等功能，需要依赖element模块
-      
-      //监听导航点击
-      element.on('nav(demo)', function(elem){
-        //console.log(elem)
-        layer.msg(elem.text());
-      });
-    });
-  </script>
-</body>
+    
+  </body>
+
 </html>
--- application/views/login.php
+++ application/views/login.php
@@ -1,30 +1,66 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en-US">
-<head>
-	<meta charset="utf-8">
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/css/login.css">
-</head>
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>登录</title>
+		<link rel="stylesheet" href="<?php echo base_url()?>public/layui/css/layui.css" media="all" />
+		<link rel="stylesheet" href="<?php echo base_url()?>public/css/login.css" />
+	</head>
 
-<body>
+	<body class="beg-login-bg">
+		<div class="beg-login-box">
+			<header>
+				<h1>后台登录</h1>
+			</header>
+			<div class="beg-login-main">
+				<form action="" class="layui-form" method="post">
+					<div class="layui-form-item">
+						<label class="beg-login-icon">
+                        <i class="layui-icon">&#xe612;</i>
+                    </label>
+						<input type="text" name="userName" lay-verify="userName" autocomplete="off" placeholder="这里输入登录名" class="layui-input">
+					</div>
+					<div class="layui-form-item">
+						<label class="beg-login-icon">
+                        <i class="layui-icon">&#xe642;</i>
+                    </label>
+						<input type="password" name="password" lay-verify="password" autocomplete="off" placeholder="这里输入密码" class="layui-input">
+					</div>
+					<div class="layui-form-item">
+						<div class="beg-pull-left beg-login-remember">
+							<label>记住帐号？</label>
+							<input type="checkbox" name="rememberMe" value="true" lay-skin="switch" checked title="记住帐号">
+						</div>
+						<div class="beg-pull-right">
+							<button class="layui-btn layui-btn-primary" lay-submit lay-filter="login">
+                            <i class="layui-icon">&#xe650;</i> 登录
+                        </button>
+						</div>
+						<div class="beg-clear"></div>
+					</div>
+				</form>
+			</div>
+			
+		</div>
+		<script type="text/javascript" src="<?php echo base_url()?>public/layui/layui.js"></script>
+		<script>
+			layui.use(['layer', 'form'], function() {
+				var layer = layui.layer,
+					$ = layui.jquery,
+					form = layui.form();
+					
+				form.on('submit(login)',function(data){
+					
+					location.href='index.html';
+					return false;
+				});
+			});
+		</script>
+	</body>
 
-	<div id="login">
-
-		<h2>RMS</h2>
-
-		<form action="<?php echo site_url('login/check')?>" method="POST">
-			<fieldset>
-				<p><label for="email">账号</label></p>
-				<p><input type="email" name="username" value=""></p>
-				<p><label for="password">密码</label></p>
-				<p><input type="password" name="pwd" value=""></p>
-				<p><input type="submit" value="登陆"></p>
-
-			</fieldset>
-		</form>
-	</div> <!-- end login -->
-
-</body>	
 </html>
